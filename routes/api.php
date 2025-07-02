f763cc990848d0dc860b363a0d53dbd02d6a37b1
adityathok <aditya0thok@gmail.com>
2025-07-02 15:12:24 +0700

    add city route

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ApiKeyMiddleware;
 
 use App\Http\Controllers\ProvinceController;
-use App\Http\Middleware\ApiKeyMiddleware;
+use App\Http\Controllers\CityController;
 
 Route::middleware(['auth:sanctum'])->group(function () {});
 
 Route::middleware([ApiKeyMiddleware::class])->group(function () {
     Route::apiResources([
-        'v1/province' => ProvinceController::class,
+        'v1/province'   => ProvinceController::class,
+        'v1/city'       => CityController::class
     ]);
 });
