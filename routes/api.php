8c1f0e096123b71944b05c61c65f5609afbc5f55
adityathok <aditya0thok@gmail.com>
2025-07-02 15:36:10 +0700

    add route Subdistrict

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,15 @@ use App\Http\Middleware\ApiKeyMiddleware;
 
 use App\Http\Controllers\ProvinceController;
 use App\Http\Controllers\CityController;
+use App\Http\Controllers\SubdistrictController;
+
 
 Route::middleware(['auth:sanctum'])->group(function () {});
 
 Route::middleware([ApiKeyMiddleware::class])->group(function () {
     Route::apiResources([
-        'v1/province'   => ProvinceController::class,
-        'v1/city'       => CityController::class
+        'v1/province'       => ProvinceController::class,
+        'v1/city'           => CityController::class,
+        'v1/subdistrict'    => SubdistrictController::class,
     ]);
 });
