094250d2b2c87fd33b8f995a43b2835cde5dea63
adityathok <aditya0thok@gmail.com>
2025-07-02 14:09:34 +0700

    change welcome

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Your IP Address: ' . $_SERVER['REMOTE_ADDR'] . '<br><small>Copyright © ' . date('Y') . ' <a href="https://velocitydeveloper.com">Velocity Developer</a></small>';
 });
