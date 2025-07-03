766eb02294622c38f91955650803dc5778e35d6a
adityathok <aditya0thok@gmail.com>
2025-07-03 08:43:07 +0700

    add: cost controller

---
+++ app/Http/Controllers/CostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\City;
+use App\Models\Subdistrict;
+
+class CostController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $status_code = 200;
+        $status_description = 'OK';
+
+        $validator = Validator::make($request->all(), [
+            'origin'            => ['required'],
+            'originType'        => ['required', 'in:city,subdistrict'],
+            'destination'       => ['required'],
+            'destinationType'   => ['required', 'in:city,subdistrict'],
+            'weight'            => ['required'],
+            'courier'           => ['required'],
+            'length'            => ['nullable', 'numeric'],
+            'width'             => ['nullable', 'numeric'],
+            'height'            => ['nullable', 'numeric'],
+            'diameter'          => ['nullable', 'numeric'],
+        ]);
+
+        if ($validator->fails()) {
+            $status_code = 400;
+            $status_description = $validator->errors();
+        }
+
+        $origin_details = [];
+        if ($request->originType == 'city') {
+            $origin_details = City::where('city_id', $request->origin)->first();
+        } else {
+            $origin_details = Subdistrict::where('subdistrict_id', $request->origin)->first();
+        }
+
+        $destination_details = [];
+        if ($request->destinationType == 'city') {
+            $destination_details = City::where('city_id', $request->destination)->first();
+        } else {
+            $destination_details = Subdistrict::where('subdistrict_id', $request->destination)->first();
+        }
+
+        $result['rajaongkir'] = [
+            'query'                 => $request->all(),
+            'origin_details'        => $origin_details,
+            'destination_details'   => $destination_details,
+            'status'                => [
+                'code'          => $status_code,
+                'description'   => $status_description,
+            ],
+            'results'               => []
+        ];
+
+        return response()->json($result);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\ApiKeyMiddleware;
 use App\Http\Controllers\ProvinceController;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\SubdistrictController;
+use App\Http\Controllers\CostController;
 
 
 Route::middleware(['auth:sanctum'])->group(function () {});
@@ -17,4 +18,5 @@ Route::middleware([ApiKeyMiddleware::class])->group(function () {
         'v1/city'           => CityController::class,
         'v1/subdistrict'    => SubdistrictController::class,
     ]);
+    Route::post('/v1/cost', [CostController::class, 'index']);
 });
