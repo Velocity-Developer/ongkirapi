071b2aa7a1f18a2f63ccdf24c3f32ba2ca41dd87
adityathok <aditya0thok@gmail.com>
2025-08-26 14:15:38 +0700

    fix: bug zip_code = 95247

--- app/Helpers/RajaOngkirHelper.php
+++ app/Helpers/RajaOngkirHelper.php
@@ -8,6 +8,12 @@ class RajaOngkirHelper
 {
     public static function getSubDistrictIdByZipCode($zipCode)
     {
+        //penanganan untuk zip_code = 95247, tidak tersedia
+        if ($zipCode == 95247) {
+            //gunakan zip_code = 95239, Tuminting kota Manado
+            $zipCode = 95239;
+        }
+
         $subDistrict = RajaongkirSubDistrict::where('zip_code', $zipCode)->first();
 
         return $subDistrict ? $subDistrict->id : null;
